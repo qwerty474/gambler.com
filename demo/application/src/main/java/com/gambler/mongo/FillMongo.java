3474a0173cbef7d9dd11dc38f593289f1f90190e
konstantin.avdonin <ksavdoni@gmail.com>
2023-02-10 10:59:52 +0300

    Защифровали пароли в монге

--- demo/application/src/main/java/com/gambler/mongo/FillMongo.java
+++ demo/application/src/main/java/com/gambler/mongo/FillMongo.java
@@ -7,6 +7,7 @@ import gmongo.user.repository.UserMngRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class FillMongo implements ApplicationRunner {
 
     private final UserMngRepository userMngRepository;
     private final ProductMngRepository productMngRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     @Override
@@ -34,19 +36,19 @@ public class FillMongo implements ApplicationRunner {
                         .setName("Oleg")
                         .setLastname("Ivanov")
                         .setLogin("test")
-                        .setPassword("test"),
+                        .setPassword(passwordEncoder.encode("test")),
                 new UserMng()
                         .setId(2)
                         .setName("Kolya")
                         .setLastname("Tapok")
                         .setLogin("kolya_tapok23@mailinator.com")
-                        .setPassword("something978"),
+                        .setPassword(passwordEncoder.encode("something978")),
                 new UserMng()
                         .setId(3)
                         .setName("Vitya")
                         .setLastname("Ivansky")
                         .setLogin("viktor7687fd@mailinator.com")
-                        .setPassword("check99978"));
+                        .setPassword(passwordEncoder.encode("check99978")));
 
         userMngRepository.insertAll(users);
     }
